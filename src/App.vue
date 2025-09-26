2e157980a161db7bbb5c71fc159d9e78c49a092d
Darin F Miller <darinfmiller@gmail.com>
2025-09-26 15:42:32 -0600

    init with vue

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { RouterLink, RouterView } from 'vue-router'
+import PageHeader from './components/PageHeader.vue'
+
+const isActive = ref(false);
+
+const toggleNav = () => {
+  isActive.value = !isActive.value;
+}
+
+</script>
+
+<template>
+  <PageHeader @toggle-nav="toggleNav" />
+
+  <RouterView />
+
+  <nav :class="['nav', { active: isActive } ]">
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+</template>
+
+<style scoped>
+
+nav {
+  background: linear-gradient(45deg, azure, lightblue);;
+  width: 100%;
+  max-height: 100%;
+  box-sizing: border-box; 
+  padding: 24px;
+  position: sticky; 
+  left: 0;
+  bottom: -100%;
+  z-index: 700;
+  overflow: auto;
+  border-radius: 8px 8px 0 0;
+  transition: .25s bottom;
+	
+   li {
+    list-style: none; 
+    padding: 16px;
+    margin:0;
+   }
+	
+   &.active {
+    bottom: 0;
+   }
+ }
+
+</style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+
+const emit = defineEmits(['toggle-nav']);
+
+const toggleNav = () => {
+  emit('toggle-nav');
+}
+
+</script>
+
+<template>
+  <header class="page-header">
+    Night 828
+    <i class="ph-bold ph-list" @click="toggleNav"></i>
+  </header>
+</template>
+
+<style scoped>
+
+header {
+  display: flex;
+  justify-content: space-between; 
+  align-items: center;
+  background: linear-gradient(45deg, azure, lightblue);
+  height: 64px;
+  padding: 16px;
+  box-sizing: border-box;
+  position: sticky; 
+  top: 0;
+  left: 0;
+  right: 0;
+  width: 100vw;
+  z-index: 600;
+  font-size: 24px;
+  a {
+   color: inherit;
+   text-decoration: none;
+  }
+ }
+
+</style>
